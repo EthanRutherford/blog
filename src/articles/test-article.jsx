f1da82ff7d09378333d20c55803186a49d889ef3
Ethan Rutherford <ethanrutherford2@gmail.com>
2020-02-29 13:19:35 -0800

    Add support for markdown in jsx

--- src/articles/test-article.jsx
+++ src/articles/test-article.jsx
@@ -6,20 +6,22 @@ export default function TestArticle({Renderer, ...props}) {
 		<Renderer
 			heroPhoto={hero}
 			title="Test article"
+			mdjsx
 			{...props}
 		>
-			<p>
-				Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-				sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-				Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
-				ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-				voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-				Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
-				deserunt mollit anim id est laborum.
-			</p>
-			<p>paragraph two</p>
-			<p>paragraph three</p>
-			<p>if it wasn't clear already, this is a placeholder until I have some real articles</p>
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+			sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+			Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
+			ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+			voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+			Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+			deserunt mollit anim id est laborum.
+
+			paragraph two
+
+			paragraph three
+
+			if it wasn't clear already, this is a placeholder until I have some real articles
 		</Renderer>
 	);
 }
--- webpack.config.js
+++ webpack.config.js
@@ -24,6 +24,7 @@ module.exports = (env) => ({
 				test: /\.css$/,
 				use: [
 					MiniCssExtractPlugin.loader,
+					"cache-loader",
 					{
 						loader: "css-loader", options: {
 							localsConvention: "camelCaseOnly",
@@ -46,6 +47,7 @@ module.exports = (env) => ({
 			}, {
 				test: /.svg$/,
 				use: [
+					"cache-loader",
 					{
 						loader: "@svgr/webpack",
 						options: {
@@ -56,13 +58,17 @@ module.exports = (env) => ({
 			}, {
 				test: /\.(list)$/,
 				use: [
+					"cache-loader",
 					"babel-loader",
 					path.resolve("./tools/article-loader.js"),
 				],
 			}, {
 				test: /\.(js|jsx)$/,
 				exclude: /node_modules/,
-				use: "babel-loader",
+				use: [
+					"cache-loader",
+					"babel-loader",
+				],
 			},
 		],
 	},
